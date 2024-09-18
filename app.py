07d4ca0ee2920c204accc0773b92d162635befd0
Igirisu861 <d.r.g.1835@gmail.com>
2024-09-17 20:26:49 -0600

    backend configurado y login

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, jsonify
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required
+from models import mongo, init_db
+from flask_bcrypt import Bcrypt
+from config import Config
+from bson.json_util import ObjectId
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+Bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
+
+init_db(app)
+
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    email = data.get('email')
+    password = data.get('password')
+
+    if mongo.db.users.find_one({'email': email}):
+        return jsonify({'msg': 'El usuario ya existe'}), 400
+    
+    hashed_password = Bcrypt.generate_password_hash(password).decode('utf-8')
+
+    result = mongo.db.users.insert_one({'email':email, 'password': hashed_password})
+    if result.acknowledged:
+        return jsonify({'msg': 'Usuario creado correctamente'}), 201
+    else:
+        return jsonify({'msg':'Hubo un error, los datos no fueron registrados'}), 400
+    
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    email = data.get('email')
+    password = data.get('password')
+    user = mongo.db.users.find_one({"email":email})
+
+    if user and Bcrypt.check_password_hash(user['password'], password):
+        access_token = create_access_token(identity=str(user["_id"]))
+        return jsonify(access_token=access_token), 200
+    else:
+        return jsonify({"msg":"Credenciales incorrectas"}), 401
+    
+
